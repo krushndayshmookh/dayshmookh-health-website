c94bc0cb44815cd0434e3965d617af48b76b23fa
krushndayshmookh <krushndayshmookh@gmail.com>
2017-05-03 17:33:28 +0530

    menubar animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-$(document).ready(function () {
-    var bodyScrollTop = $("body").scrollTop();
-    //if (navDummyOffset.top < )
+//$(document).ready(function () {
+//   var bodyScrollTop = $("body").scrollTop();
+//if (navDummyOffset.top < )
 
+//});
+
+
+$(window).scroll(function () {
+    if (($(window).scrollTop() > 445) && ($("#nav-main").css("display") === "none")) {
+        $("#nav-main").slideDown();
+    } else if (($(window).scrollTop() < 445) && ($("#nav-main").css("display") === "block")) {
+        $("#nav-main").slideUp();
+    } else {
+        $("#nav-main").css("display") = "none";
+    }
 });
